f2b5ae0dc0d68ba16d3985eca558002424e3583d
luizengdev <luizengdev@gmail.com>
2024-06-07 17:31:33 -0300

    feat: add createOpening handler logic

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,17 @@
 package handler
+
+import (
+	"github.com/luizengdev/gopportunities/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+// InitializeHandler initializes the handler and its dependencies globally
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
--- schemas/opening.go
+++ schemas/opening.go
@@ -1,15 +1,30 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type Opening struct {
 	gorm.Model
-	Role     string `json:"role" gorm:"not null"`
-	Company  string `json:"company" gorm:"not null"`
-	Location string `json:"location" gorm:"not null"`
-	Remote   bool   `json:"remote" gorm:"not null"`
-	Link     string `json:"link" gorm:"not null"`
-	Salary   int64  `json:"salary" gorm:"not null"`
+	Role     string
+	Company  string
+	Location string
+	Remote   bool
+	Link     string
+	Salary   int64
+}
+
+type OpeningResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
 }
