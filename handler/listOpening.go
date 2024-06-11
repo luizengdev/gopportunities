ebbacb04fca560f01f4d16ca7ac84b36d45b8059
luizengdev <luizengdev@gmail.com>
2024-06-11 17:55:07 -0300

    docs: add documentation for List Openings

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -8,6 +8,16 @@ import (
 	"github.com/luizengdev/gopportunities/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary List opening
+// @Description List all job openings
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningsResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [get]
 func ListOpeningsHandler(ctx *gin.Context) {
 	openings := []schemas.Opening{}
 	if err := db.Find(&openings).Error; err != nil {
